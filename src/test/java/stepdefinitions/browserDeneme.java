f1208f8c19a8820e0d640d990844d83979832901
Nevzat <nevzatcelik151907@gmail.com>
2024-03-12 09:51:16 +0100

    fourth commit

---
+++ src/test/java/stepdefinitions/browserDeneme.java
@@ -0,0 +1,12 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import utils.BrowserDriver;
+
+public class browserDeneme {
+
+    @Given("Kullanici belirledigi {string} url e gider")
+    public void kullanici_belirledigi_url_e_gider(String url) {
+        BrowserDriver.getBrowserDriver().get(url);
+    }
+}
